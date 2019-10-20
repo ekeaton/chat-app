3bffc4a0a93491fa2e42486382090450fee01d6e
Estelle K <e.keaton@live.com>
2019-10-20 12:52:04 -0700

    connected chat-app-api

---
+++ src/config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/110879dd-259f-4d4d-9968-332ae632983a/token";
+const instanceLocator = "v1:us1:110879dd-259f-4d4d-9968-332ae632983a";
+
+export { tokenUrl, instanceLocator}
